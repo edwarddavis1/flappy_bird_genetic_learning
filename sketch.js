b410b0ef6c33ad7708fdd15ae4c0e5e792d26f42
Ed <edwarddavis@hotmail.co.uk>
2020-08-18 17:00:47 +0100

    Got a single pipe pair moving

--- sketch.js
+++ sketch.js
@@ -1,27 +1,36 @@
 class Pipe {
-    init(gap, gap_loc, speed, width) {
-        this.gap = gap;
-        this.gap_loc = gap_loc;
-        this.speed = speed;
-        this.width = width;
-        this.x = windowWidth;
-        this.y_upper = windowHeight - gap_loc - (gap / 2);
-        this.y_lower = windowHeight - this.y_upper;
-    }
+  constructor(gap, gapLoc, speed, width) {
+    this.gap = gap;
+    this.gapLoc = gapLoc;
+    this.speed = speed;
+    this.width = width;
+    this.x = windowWidth;
+    this.yUpper = windowHeight - gapLoc - (gap / 2);
+    this.yLower = windowHeight - this.yUpper;
+
+  }
+
+  step() {
+    // Steps the pipes along the screen
+    fill(51);
+    // Lower pipe
+    rect(this.x, 0, this.width, this.yLower);
+    // Upper pipe
+    rect(this.x, this.yLower + this.gap, this.width, this.yUpper);
+    // Move
+    this.x -= this.speed;
+  }
+
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255, 255, 255);
-
+  background(255);
+  pipes = new Pipe(100, 400, 1, 50);
 }
 
 function draw() {
-    background(255, 255, 255)
+  background(255);
 
-    // Pipes
-    pipe = Pipe(100, 1, 50)
-
-    // Upper pipe
-    rect(pipe.x, pipe.y_upper, windowHeight - pipe.y_upper, pipe.width)
+  pipe.step();
 }
