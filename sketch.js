34e1b1503a90b75ee7eb06e7a0784266d5587976
Edward Davis <52565961+edwarddavis1@users.noreply.github.com>
2020-08-18 19:35:15 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,36 @@
 class Pipe {
   constructor(gap, gapLoc, speed, width) {
     this.gap = gap;
-    this.gapLoc = gapLoc;
+    this.gapLoc = random(0, windowHeight);
     this.speed = speed;
     this.width = width;
     this.x = windowWidth;
-    this.yUpper = windowHeight - gapLoc - (gap / 2);
+    this.yUpper = windowHeight - this.gapLoc - (this.gap / 2);
     this.yLower = windowHeight - this.yUpper;
 
+  }
+
+  reset() {
+    // Starts the pipe pair from the right of the window with new gap
+    this.x = windowWidth;
+    this.gapLoc = random(0, windowHeight);
   }
 
   step() {
     // Steps the pipes along the screen
     fill(51);
     // Lower pipe
+    this.yLower = this.gapLoc - (this.gap / 2);
     rect(this.x, 0, this.width, this.yLower);
     // Upper pipe
-    rect(this.x, this.yLower + this.gap, this.width, this.yUpper);
+    rect(this.x, this.yLower + this.gap, this.width,
+          windowHeight - this.gap - this.yLower);
     // Move
-    this.x -= this.speed;
+    if (this.x > -this.width) {
+      this.x -= this.speed;
+    } else {
+      this.reset();
+    }
   }
 
 }
@@ -26,7 +38,7 @@ class Pipe {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
-  pipes = new Pipe(100, 400, 1, 50);
+  pipe = new Pipe(100, 400, 2, 50);
 }
 
 function draw() {
